c2a41b1aca8ae184e4801213ee92e504e9c51bb0
suharbramar <suhar_bramar@yahoo.com>
2021-12-30 10:23:07 +0700

    Jumping on the clouds

---
+++ ProblemSolving/src/JumpingOnTheClouds.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class JumpingOnTheCloudsResult {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Write your code here
+
+        int count = 0;
+        for (int i = 0; i < c.size() - 1; i++) {
+            if (c.get(i) == 0) i++;
+            count++;
+        }
+
+        return count;
+
+
+    }
+
+}
+
+public class JumpingOnTheClouds {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = JumpingOnTheCloudsResult.jumpingOnClouds(c);
+
+        System.out.println(result);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
