db9c325dc3cea3fd00cfe4bec05a92e66b2bdfc7
suharbramar <suhar_bramar@yahoo.com>
2022-01-25 20:52:39 +0700

    marktoys and two strings

--- JavaBasicHackerRank/src/com/java/practice/lambda/JavaBasicLambda1.java
+++ JavaBasicHackerRank/src/com/java/practice/lambda/JavaBasicLambda1.java
@@ -1,18 +1,36 @@
 package com.java.practice.lambda;
 
-interface MyFunction{
+//functional interface only can have single abstract method
+interface MyFunctionalInterface{
+    default void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+    static int getNumber(){
+        return  1;
+    }
+
     String printText(String text1, String text2);
 
 }
 
 
-
 public class JavaBasicLambda1 {
-    public static void main(String args[]){
-       MyFunction myFunction = (text1, text2) -> {
-           return text1 + text2 + " Test lambda";
-       };
-       System.out.println(myFunction.printText("Hello Word", "lambda"));
 
+    // pass functionality as method argument to functional interface
+    public static MyFunctionalInterface doPrint(){
+       return (text1, text2) -> "Basic lambda "+text1 +" "+text2;
+    }
+
+
+    public static void main(String args[]){
+        MyFunctionalInterface myFunctionalInterface = doPrint();
+        myFunctionalInterface.printText("Exp1", "Exp2");
+
+//       MyFunctionalInterface myFunctionalInterface = ((text1, text2) -> {
+//          // System.out.println(text1+" "+text2);
+//           return "Basic lambda "+text1 +","+text2;
+//       });
+//
+       System.out.println(myFunctionalInterface.printText("expression1", "expression2"));
     }
 }
